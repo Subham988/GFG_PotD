b0c059a316ddc2fedfabe553953e5f461241665f
Subham988 <83450579+Subham988@users.noreply.github.com>
2023-04-12 12:31:04 +0530

    Create Dominant_Pairs.cpp

---
+++ 12th_April/Dominant_Pairs.cpp
@@ -0,0 +1,20 @@
+
+#include<vector>
+class Solution{
+public:
+    int dominantPairs(int n,vector<int> &arr){
+         //code here
+        int count=0;
+        sort(arr.begin(), arr.begin()+(n/2));
+        sort(arr.begin()+(n/2), arr.end());
+        
+        int j=n/2;
+        for(int i=0 ; i< n/2 ; i++){
+            while(j<n && arr[i]>=arr[j]*5){
+                j++;
+            }
+            count += j-n/2;
+        }
+        return count;
+    }
+};
