160a4fad586d76268b8c8d3e99f1eebb2a861477
Subham988 <subhamagrahari988@email.com>
2023-04-15 16:28:35 +0530

    Create Find Total Time Taken.cpp

---
+++ 15th_April/Find Total Time Taken.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int totalTime(int n, vector<int> &arr, vector<int> &time) {
+        // code here
+        unordered_set<int> list;
+        int t = -1;
+        int prevT = 0;
+        for (int i = 0; i < n; i++) {
+          
+            if (list.find(arr[i]) == list.end()) {
+                list.insert(arr[i]);
+                t++;
+            }
+            else {
+                t += time[arr[i]-1];
+            }
+        }
+        return t;
+    }
+};
