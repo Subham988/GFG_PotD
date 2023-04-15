fc1141d1bb55af3b3ebb7ae04d9c8d3e9dd9cef6
Subham988 <subhamagrahari988@email.com>
2023-04-15 16:30:21 +0530

    Create Find Total Time Taken.java

---
+++ 15th_April/Find Total Time Taken.java
@@ -0,0 +1,21 @@
+class Solution
+{
+ public static long totalTime(int n,int arr[],int time[])
+    {
+        Set<Integer> list = new HashSet<>();
+        int t = -1;
+        int prevT=0;
+        for(int i=0 ; i < n; i++){
+          
+            if(!list.contains(arr[i])){
+                list.add(arr[i]);
+              t++;
+            }else{
+                t += time[arr[i]-1];
+            }
+        }
+
+        return t;
+    }
+
+}
