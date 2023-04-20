af07868b32db64e2c06abfcdf3fdef6934f9d19f
Subham Agrahari <83450579+Subham988@users.noreply.github.com>
2023-04-20 12:41:49 +0530

    Update Bheem Wants Ladoos.java

--- 20th_April/Bheem Wants Ladoos.java
+++ 20th_April/Bheem Wants Ladoos.java
@@ -1,69 +1,43 @@
 class Solution{
-    static int ladoos(Node root, int home, int k){
-        // Your code goes here
-           Map<Node, Node> map = new HashMap<>();
-        Queue<Node> q = new LinkedList<>();
-        map.put(root, null);
-        q.add(root);
-        Node bheemsHome = null;
-        
-        while(!q.isEmpty()){
-            int size = q.size();
-            while(size-- > 0){
-                Node curr = q.poll();
-                if(curr.data == home){
-                    bheemsHome = curr;
-                }
-                if(curr.left != null){
-                    map.put(curr.left, curr);
-                    q.add(curr.left);
-                }
-                
-                if(curr.right != null){
-                    map.put(curr.right, curr);
-                    q.add(curr.right);
-                }
-            }
-        }
-        
-        
-         Queue<Pair> bheem = new LinkedList<>();
-        bheem.add(new Pair(bheemsHome, 0));
-        // bheem.add(bheemsHome);
-        int ans = 0;
-        Set<Node> visited = new HashSet<>();
-        visited.add(bheemsHome);
-        
-        while(!bheem.isEmpty()){
-            int size = bheem.size();
-            while(size-- > 0){
-                Pair curr = bheem.poll();
-                ans += curr.node.data;
-                if(curr.node.left != null && curr.dist < k && !visited.contains(curr.node.left)){
-                    bheem.add(new Pair(curr.node.left, curr.dist+1));
-                    visited.add(curr.node.left);
-                }
-                
-                if(curr.node.right != null && curr.dist < k && !visited.contains(curr.node.right)){
-                    bheem.add(new Pair(curr.node.right, curr.dist+1));
-                    visited.add(curr.node.right);
-                }
-                
-                if(map.get(curr.node) != null && curr.dist < k && !visited.contains(map.get(curr.node))){
-                    bheem.add(new Pair(map.get(curr.node), curr.dist+1));
-                    visited.add(map.get(curr.node));
-                }
-            }
-        }
-        return ans;
-    }
-}
-class Pair{
-    Node node;
-    int dist;
+    static int sum;
     
-    Pair(Node n, int d){
-        node = n;
-        dist = d;
+    static void add_subtree(Node n, int dist){
+        if( (n==null) || (dist<0) ) return;
+        sum += n.data;
+        add_subtree(n.left, dist-1);
+        add_subtree(n.right, dist-1);
+    }
+    
+    static int traverse(Node n, int target, int k){
+        if(n==null) return -1;
+        if(n.data==target)
+        {
+            add_subtree(n, k);
+            return k-1;
+        }
+        
+        int dist = traverse(n.left, target, k);
+        if(dist>-1)
+        {
+            sum += n.data;
+            add_subtree(n.right, dist-1);
+            return dist-1;
+        }
+        
+        dist = traverse(n.right, target, k);
+        if(dist>-1)
+        {
+            sum += n.data;
+            add_subtree(n.left, dist-1);
+            return dist-1;
+        }
+        
+        return -1;
+    }
+    
+    static int ladoos(Node root, int home, int k){
+        sum = 0;
+        traverse(root, home, k);
+        return sum;
     }
 }
