97ef72d8a6e243d4bc9b5793c5fa5eb79fad0084
Subham Agrahari <83450579+Subham988@users.noreply.github.com>
2023-04-23 15:27:13 +0530

    Update Minimum Number.java

--- 23th_April/Minimum Number.java
+++ 23th_April/Minimum Number.java
@@ -4,12 +4,10 @@ class Solution {
         return gcd(y , x%y);
     }
     public static int minimumNumber(int n, int[] arr) {
-        // code here
         int g = gcd(arr[0] , arr[1]);
         for(int i=1 ; i < n-1 ; i++){
             g = gcd(g , gcd(arr[i] , arr[i+1]));
         }
-        
         return g;
     }
 }
