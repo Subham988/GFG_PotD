e355f4adee0cf83eb878c162acbfdd68f6990339
Subham Agrahari <83450579+Subham988@users.noreply.github.com>
2023-05-03 20:38:37 +0530

    Update Make Palindrome.cpp

--- May/3rd_May/Make Palindrome.cpp
+++ May/3rd_May/Make Palindrome.cpp
@@ -1,15 +1,51 @@
+
+
 class Solution{
 public:
-    bool makePalindrome(int n,vector<string> &arr){
-        // Code here
-         unordered_map<string,int> mp;
-        for(auto it: arr)mp[it]++;
-        for(auto it: arr){
-            string rs = it;
-            reverse(rs.begin(),rs.end());
-            if(mp[rs]==mp[it])continue;
-            else return false;
+     bool makePalindrome(int n,vector<string> &arr){
+        map<string,int> mp;
+        
+        for(int i = 0;i < arr.size(); i++)
+        {
+            string b = arr[i];
+            reverse(b.begin(),b.end());
+            if(mp.find(b) == mp.end())
+            {
+                mp[arr[i]] = 1;
+            }
+            else
+            {
+                mp[b]++;
+            }
         }
-        return true;
+        
+        if(n%2 == 0)
+        {
+            for(auto x : mp)
+            {
+                if(x.second%2 != 0)return false;
+            }
+            return true;
+        }
+        else 
+        {
+            bool check = false;
+            for(auto x : mp)
+            {
+                if(x.second%2 != 0 && check == false)
+                {
+                    string tmp = x.first;
+                    reverse(tmp.begin(),tmp.end());
+                    if(x.first != tmp)return false;
+                    else check = true;
+                }
+                else if(x.second%2 != 0 && check == true)
+                {
+                    return false;
+                }
+            }
+            return true;
+            
+        }
     }
 };
